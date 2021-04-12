73e4f0716c89a89a6b6e70e967a14de4d11392b7
Helena Kuehnle <hekueh@farm-ui2.physik.uzh.ch>
2021-04-12 16:13:33 +0200

    Added fit to distribution plot and edited the loop file.

--- LbToLclnu_fit_binscheme_extractres.py
+++ LbToLclnu_fit_binscheme_extractres.py
@@ -1,19 +1,34 @@
 import sys, os
 import numpy as np
+import scipy as sc
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 ###############################
 
+def g(x, mu, sigma):
+    return 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (x - mu)**2 / (2 * sigma**2))
+
 #read results from files
-n = 1
+n = 100
+coeff = 'CVR'
+scheme = 6
+FF = 'All'
+TCVR = 0
 uncert = []
 val = []
 for i in range(n):
-    with  open('plots/results_CVR_' + str(i) + '_Scheme6_7500000_None_toy.txt','r') as txt:
+    with  open('plots/results_' + coeff + '_' + str(i+100) + '_Scheme' + str(scheme) + '_7500000_' + FF + '_toy.txt','r') as txt:
         data = txt.read()
     print(data)
     data = data.split()
-    val.append(float(data[1]))
-    uncert.append(float(data[2]))
+    ii = data.index(coeff)
+    val.append(float(data[ii+1]))
+    uncert.append(float(data[ii+2]))
+    print(ii, data[ii+1], data[ii+2])
+
+    #i1 = data.index('has_parameters_at_limit')
+    #i2 = data.index('has_posdef_covar')
+    
 
 print('Values for fitted CVR:')
 print(val)
@@ -23,16 +38,53 @@ print(uncert)
 print('Finished composing the results!')
 
 #plot the results
+fig, ax = plt.subplots(2)
+ax[0].hist(val)
+fig.suptitle('Distribution of values and Hessian errors of CVR for '+ str(n) +' Fits in Binning Scheme '+ str(scheme))
+ax[0].set_xlabel('CVR value')
+ax[0].set_ylabel('Number of Fits [#]')
 
-fig, ax = plt.subplots()
-ax = plt.hist(val)
-ax = plt.title('Distribution of values of CVR for 100 Fits')
-ax = plt.xlabel('CVR value')
+ax[1].hist(uncert)
+ax[1].set_xlabel('Hessian Error')
+ax[1].set_ylabel('Number of Fits [#]')
+#ax[1].set_xticks([5.55e-4,5.56e-4,5.57e-4])
+#ax[1].set_xticklabels(['5.55e-4','5.56e-4','5.57e-4'])
+#plt.show()
 
-fig1, ax = plt.subplots()
-ax = plt.hist(uncert)
-ax = plt.title('Distribution of uncertainties of CVR for 100 Fits')
-ax = plt.xlabel('Hess err')
 
+#fig, ax = plt.subplots()
+#gauss = np.random.normal(loc = TCVR, scale = 1, size = np.size(np.array(val)))
+#newval = (np.array(val)-TCVR)/np.array(uncert)
+#ax.hist(gauss, alpha = 0.5, label = 'Normal (0,1)')
+#countnew, binsnew, ignored = ax.hist(newval, alpha = 0.5, label = 'Values')
+#ax.legend()
+#ax.set_xlabel('Hessian Error')
+#ax.set_ylabel('Bincount/Error')
+#ax.set_title('(CVR - TrueCVR)/Hess')
+#plt.show()
+
+plt.figure()
+val = np.array(val)
+newval = (np.array(val)-TCVR)/np.array(uncert)
+count, bins, ignored = plt.hist(newval, 30, density=True, label = 'Data', alpha = 0.8)
+x = np.linspace(np.min(newval), np.max(newval), len(count))
+print(x, count, bins)
+#popt, pcov = curve_fit(g, x, count, p0 = [np.mean(count),np.std(count)])
+popt, pcov = curve_fit(g, x, count, p0 = [TCVR,1])
+print('Fitted Mean and Sigma:',popt)
+plt.plot(x, g(x,*popt), label = 'Fitted Normal', color = 'r')
+plt.plot(x, g(x,TCVR,1), label = 'Normal N(0,1)')
+plt.legend()
+plt.xlabel('Hessian Error')
+plt.ylabel(coeff + ' - True' + coeff + ')/Hess [%/100]')
+plt.title('(' + coeff + ' - True' + coeff + ')/Hess of binscheme '+ str(scheme) + ' and ' + FF + ' FF floated' )
 plt.show()
 
+#evaluate mean and median
+meanval = np.mean(val)
+meanunc = np.mean(uncert)
+medianval = np.median(val)
+medianuncert = np.median(uncert)
+
+print('Mean: ', meanval, meanunc)
+print('Median: ', medianval, medianuncert)
--- LbToLclnu_fit_binscheme_loop.py
+++ LbToLclnu_fit_binscheme_loop.py
@@ -2,13 +2,7 @@ import sys, os
 
 n = 100
 for i in range(n):
-    command = 'python LbToLclnu_fit_binscheme.py -f CVR -b Scheme6 -s '+ str(i+100)
+    command = 'python LbToLclnu_fit_binscheme.py -f CT -b Scheme6 -inter 10 -p False -e None -s '+ str(i+100)
     os.system(command)
-    #cm1 = 'rm results_CVR_'+str(i)+'_Scheme6_7500000_None_toy_data.pdf'
-    #cm2 = 'rm results_CVR_'+str(i)+'_Scheme6_7500000_None_toy_fit.pdf'
-    #cm3 = 'rm results_CVR_'+str(i)+'_Scheme6_7500000_None_toy_pull.pdf'
-    #os.system(cm1)
-    #os.system(cm2)
-    #os.system(cm3)
 
 print('Finished with '+ str(n) +' fits!') 
